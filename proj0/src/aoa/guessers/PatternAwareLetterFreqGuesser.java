c629f585ee6573be347f437f96614c287b6be7e4
SYYoung <r38411@yahoo.com>
2024-12-02 17:40:15 +0800

    build a helper function. works for PatternAwareLetterFreqGuesser

--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -33,40 +33,6 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
         return matchedWord;
     }
 
-    private Map<Character, Integer> getFreqMapThatMatchesPattern(List<String> matchedWords) {
-        // TODO: Fill in this method.
-        Map<Character, Integer> freqMap = new TreeMap<>();
-        for (String s: matchedWords) {
-            for (int i=0; i<s.length(); i++) {
-                Character c = s.charAt(i);
-                if (freqMap.containsKey(c)) {
-                    freqMap.put(c, freqMap.get(c)+1);
-                } else {
-                    freqMap.put(c, 1);
-                }
-            }
-        }
-        return freqMap;
-    }
-
-    private Map<Character, Integer> sortedFrequencyMap(Map<Character, Integer> originalMap) {
-        List<Map.Entry<Character, Integer>> list =
-                new LinkedList<Map.Entry<Character, Integer>>(originalMap.entrySet());
-        // sort the list
-        Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
-            public int compare(Map.Entry<Character, Integer> o1,
-                               Map.Entry<Character, Integer> o2) {
-                return (o1.getValue().compareTo(o2.getValue()) * -1);
-            }
-        });
-        // put data from sorted list to a new hashmap
-        HashMap<Character, Integer> temp = new LinkedHashMap<Character, Integer>();
-        for (Map.Entry<Character, Integer> aa: list) {
-            temp.put(aa.getKey(), aa.getValue());
-        }
-        return temp;
-    }
-
     @Override
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
@@ -75,8 +41,8 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
         Map<Character, Integer> freqMap = new TreeMap<>();
         Map<Character, Integer> sortedFreqMap = new HashMap<>();
         List<String> matchedWords = keepOnlyWordsThatMatchPattern(pattern);
-        freqMap = getFreqMapThatMatchesPattern(matchedWords);
-        sortedFreqMap = sortedFrequencyMap(freqMap);
+        freqMap = LFGHelper.getFreqMapThatMatchesPattern(matchedWords);
+        sortedFreqMap = LFGHelper.sortedFrequencyMap(freqMap);
         if (freqMap.isEmpty())
             return '?';
         // get the keys from the sorted map
