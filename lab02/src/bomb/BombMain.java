ecf7a208ef08710efac3611257bc9d8f8fe4cc68
SYYoung <r38411@yahoo.com>
2024-12-10 19:48:19 +0800

    done: Bomb

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -2,6 +2,8 @@ package bomb;
 
 import common.IntList;
 
+import java.util.Arrays;
+
 public class BombMain {
     public static void main(String[] args) {
         int phase = 2;
@@ -15,10 +17,20 @@ public class BombMain {
             b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            IntList phase1List = IntList.of(0,9,3,0,8);
+            //b.phase1(null); // Figure this out too
+            b.phase1(phase1List);
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            // construct the password string
+            String[] testStrArr = new String[]{"1","2","3"};
+            String testStr = Arrays.toString(testStrArr);
+
+            String repeatStr = "-81201430 ";
+            int passwdLength = 1340;
+            String passwd = repeatStr.repeat(passwdLength);
+            //b.phase2("Figure this out. I wonder where the phases are defined...");
+            b.phase2(passwd);
         }
     }
 }
