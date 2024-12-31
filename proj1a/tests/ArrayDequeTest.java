389f069ee92b22cad4e1ca51b24022164d1a5ef8
SYYoung <r38411@yahoo.com>
2024-12-31 15:38:24 +0800

    Done: isEmptyAndSizeTest()

--- proj1a/tests/ArrayDequeTest.java
+++ proj1a/tests/ArrayDequeTest.java
@@ -67,4 +67,50 @@ public class ArrayDequeTest {
         ad.addFirst("r");
         assertThat(ad.toList()).containsExactly("r","p","o","n","m","f","c","a","b","d","e","g","h","Z","w","x","y").inOrder();
     }
+
+    @Test
+    public void isEmptyAndSizeTest() {
+        ArrayDeque<String> ad = new ArrayDeque<>();
+        /* test the queue is empty */
+        assertWithMessage("expected: empty list").that(ad.isEmpty()).isTrue();
+        assertWithMessage("expected: size is 0").that(ad.size()).isEqualTo(0);
+
+        ad.addLast("a");
+        ad.addLast("b");
+        ad.addFirst("c");
+        ad.addLast("d");
+        ad.addLast("e");
+        ad.addFirst("f");
+        ad.addLast("g");
+        ad.addLast("h");
+        /* test the list */
+        assertWithMessage("expected: not empty list").that(ad.isEmpty()).isFalse();
+        assertWithMessage("expected: size is 8").that(ad.size()).isEqualTo(8);
+
+        /* this will cause resize */
+        ad.addLast("Z");
+        assertWithMessage("expected: not empty list").that(ad.isEmpty()).isFalse();
+        assertWithMessage("expected: size is 9").that(ad.size()).isEqualTo(9);
+
+        ad.addLast("w");
+        ad.addLast("x");
+        ad.addLast("y");
+        ad.addFirst("m");
+        ad.addFirst("n");
+        ad.addFirst("o");
+        ad.addFirst("p");
+        /* test again */
+        assertWithMessage("expected: not empty list").that(ad.isEmpty()).isFalse();
+        assertWithMessage("expected: size is 16").that(ad.size()).isEqualTo(16);
+
+        /* should resize again */
+        ad.addFirst("r");
+        assertWithMessage("expected:not empty list").that(ad.isEmpty()).isFalse();
+        assertWithMessage("expected: size is 17").that(ad.size()).isEqualTo(17);
+    }
+
+    @Test
+    public void sizeTest() {
+
+    }
 }
