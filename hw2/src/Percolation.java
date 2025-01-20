4e5b6b97448cc4cb3462087c1e980a1f3bf03e75
SYYoung <r38411@yahoo.com>
2025-01-20 17:05:40 +0800

    Done: implement full()

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -8,24 +8,30 @@ public class Percolation {
     private int[][] grid;
     private int totalOpen;
     private int dimension;
+    // virtual top node and virtual bottom node
+    private int virtualTop;
+    private int virtualBot;
     // values for OPEN, BLOCKED and FULL
     private final int OPEN = 1;
     private final int BLOCKED = 0;
     private final int FULL = 2;
-    // virtual top node and virtual bottom node
+
 
     public Percolation(int N) {
         // TODO: Fill in this constructor.
         if (N <= 0)
             throw new IllegalArgumentException();
         dimension = N;
-        wuf = new WeightedQuickUnionUF(N * N);
+        wuf = new WeightedQuickUnionUF(N * N + 2);
         grid = new int[N][N];
         // initialize all the grids as BLOCKED
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++)
                 grid[i][j] = BLOCKED;
         totalOpen = 0;
+        // set up virtualTop and virtualBot
+        virtualTop = N * N;
+        virtualBot = N * N + 1;
     }
 
     public void open(int row, int col) {
@@ -34,6 +40,8 @@ public class Percolation {
             throw new IndexOutOfBoundsException();
         grid[row][col] = OPEN;
         connect2Adj(row, col);
+        connect2VirtualTop(row, col);
+        totalOpen++;
     }
 
     public boolean isOpen(int row, int col) {
@@ -43,7 +51,10 @@ public class Percolation {
 
     public boolean isFull(int row, int col) {
         // TODO: Fill in this method.
-        return false;
+        if (wuf.connected(rc21d(row, col), virtualTop))
+            return true;
+        else
+            return false;
     }
 
     public int numberOfOpenSites() {
@@ -92,10 +103,34 @@ public class Percolation {
         }
     }
 
+    private void connect2VirtualTop(int row, int col) {
+        if (row != 0)
+            return;
+        wuf.union(rc21d(row, col), virtualTop);
+    }
+
+
     public static void main(String[] args) {
         Percolation per = new Percolation(5);
-        per.open(3, 4);
-        per.open(2,4);
-        boolean connect = per.wuf.connected(per.rc21d(3,4), per.rc21d(2,4));
+        int r1, r2, c1, c2;
+        r1 = 3; c1 = 4; r2 = 2; c2 = 4;
+        per.open(r1, c1);
+        per.open(r2, c2);
+        boolean connect = per.wuf.connected(14,19);
+        r1=2; c1 = 2; r2 = 2; c2 = 3;
+        per.open(r1, c1);
+        per.open(r2, c2);
+        // now (12,1) should be connected. also (13,1) should be connected
+        connect = per.wuf.connected(12, 13);
+        connect = per.wuf.connected(13, 14);
+        r1 = 0; c1 = 2; r2 = 1; c2 = 2;
+        per.open(r1, c1);
+        per.open(r2, c2);
+        connect = per.wuf.connected(2, 7);
+        connect = per.wuf.connected(7, 12);
+        r1 = 2; c1 = 2;
+        per.open(r1, c1);
+        boolean fullOrNot = per.isFull(r1, c1); // should be full
+        fullOrNot = per.isFull(3, 4);
     }
 }
