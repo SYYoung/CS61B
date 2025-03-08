e383401eb883a8f700307d1ae17a8adc8f45a6ac
SYYoung <r38411@yahoo.com>
2025-03-08 17:13:00 +0800

    KnightWorld.java: finish initBoard() which draws the initial board

--- lab12/src/knightworld/KnightWorld.java
+++ lab12/src/knightworld/KnightWorld.java
@@ -2,6 +2,7 @@ package knightworld;
 
 import tileengine.TERenderer;
 import tileengine.TETile;
+import tileengine.Tileset;
 
 /**
  * Draws a world consisting of knight-move holes.
@@ -10,11 +11,26 @@ public class KnightWorld {
 
     private TETile[][] tiles;
     // TODO: Add additional instance variables here
+    private int wd, ht;
+
+    private void initBoard(int width, int height) {
+        wd = width;
+        ht = height;
+        tiles = new TETile[wd][ht];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x][y] = Tileset.GRAY_UNLOCKED_DOOR;
+            }
+        }
+    }
 
     public KnightWorld(int width, int height, int holeSize) {
         // TODO: Fill in this constructor and class, adding helper methods and/or classes as necessary to draw the
         //  specified pattern of the given hole size for a window of size width x height. If you're stuck on how to
         //  begin, look at the provided demo code!
+        // 1. fill in the board with grid lines
+        initBoard(width, height);
+
     }
 
     /** Returns the tiles associated with this KnightWorld. */
